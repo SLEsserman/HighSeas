64a61f2acb8f81ee1011cec27810fb65895bbb28
Samuel Esserman <samessermanlambda@gmail.com>
2020-07-20 10:26:44 -0400

    HighSeas

---
+++ HS-APP/Views/Compositional/HomePageCompositionalController.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageCompositionalController.swift
+//  HS-APP
+//
+//  Created by Samuel Esserman on 7/20/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import SwiftUI
+
+//class HomePageCompositionalController: UICollectionViewController {
+//
+//    init() {
+//
+//        let layout = UICollectionViewCompositionalLayout {
+//
+//        }
+//    }
+//}
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//}
